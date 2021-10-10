a58806dd0e7f18b8d0a32b510f8718839ebc729a
rafaellamonier <rafaellamonier17@hotmail.com>
2021-10-10 07:57:10 -0300

    update

--- simple_sort.c
+++ simple_sort.c
@@ -2,31 +2,45 @@
 
 int main(){
     int a,b,c;
-    int x, y, z, temp;
+    int temp;
+    // int x, y, z, temp;
+    int v[3];
 
     scanf("%d %d %d", &a, &b, &c);
-    x = a;
-    y = b;
-    z = c;
-
+    v[0] = a;
+    v[1] = b;
+    v[2] = c;
+    
+    // solução usando condicional
+    /*
     if (x>y) {
         temp = x;
         x = y;
         y = temp;
-    }
-    if (x>z) {
+    }else if (x>z) {
         temp = x;
         x = z;
         z = temp;
-    }
-    if (y>z) {
+    }else if (y>z) {
         temp = y;
         y = z;
         z = temp;
     }
+    */
 
-    printf("\n%d\n%d\n%d\n", x, y, z);
-    printf("\n%d\n%d\n%d\n", a, b, c);
+    // solução usando for
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if(v[i]<v[j]){
+                temp = v[i];
+                v[i] = v[j];
+                v[j] = temp;
+            }
+        }
+    }
+
+    // printf("\n%d\n%d\n%d\n", x, y, z);
+    printf("\n%d\n%d\n%d\n", v[0], v[1], v[2]);
     
     return 0;
 }
