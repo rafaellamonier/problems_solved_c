640f7d775a12d62681377e8fd9bcd36b93ca5880
rafaellamonier <rafaellamonier17@hotmail.com>
2021-10-10 16:27:03 -0300

    New problem solved - add tree min

--- soma_tres_menores.c
+++ soma_tres_menores.c
@@ -19,26 +19,6 @@ int main() {
 		soma = a+b+c;  
 		printf("A soma entre os tres menores números é %d\n", soma);
 	}
-
-	// solução mais légivel
-	int numero_um, numero_dois, numero_tres, numero_quatro, soma_solucao_dois;
-
-	printf("Digite 4 números: \n");
-	scanf("%d %d %d %d", &numero_um, &numero_dois, &numero_tres, &numero_quatro);
-	
-	if(numero_um>numero_dois && numero_um>numero_tres && numero_um>numero_quatro){
-		soma_solucao_dois = numero_dois+numero_tres+numero_quatro;
-		printf("A soma entre os tres menores números é %d\n", soma_solucao_dois);
-	}else if(numero_dois>numero_um && numero_dois>numero_tres && numero_dois>numero_quatro){
-		soma_solucao_dois = numero_um+numero_tres+numero_quatro;
-		printf("A soma entre os tres menores números é %d\n", soma_solucao_dois);
-	}else if(numero_tres>numero_um && numero_tres>numero_dois && numero_tres>numero_quatro){
-		soma_solucao_dois = numero_um+numero_dois+numero_quatro; 
-		printf("A soma entre os tres menores números é %d\n", soma_solucao_dois);
-	}else if(numero_quatro>numero_um  && numero_quatro>numero_dois && numero_quatro>numero_tres){
-		soma_solucao_dois = numero_um+numero_dois+numero_tres;  
-		printf("A soma entre os tres menores números é %d\n", soma_solucao_dois);
-	}
 	
 	return 0;
 }
